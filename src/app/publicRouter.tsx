dea4a87fae2098228b4639336486fedad15749ef
FranciscoGXX <gabrielxx188@gmail.com>
2025-12-06 10:43:07 -0400

    Configurando flowbite en el proyecto

--- src/app/publicRouter.tsx
+++ src/app/publicRouter.tsx
@@ -1,22 +1,14 @@
 import { Route, Routes,Link } from 'react-router-dom'
 
 //Pages
-import Home from '../page/public/Home/Home'
+import {Home} from '../page/public/Home/Home'
 
 
 const HomePage = () => (<Home />)
 
 const publicRouter = () => {
     return (
-        <div>
-
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
-            </nav>
-
-        </div>
+        <HomePage />
     )
 }
 
--- src/page/public/Home/Home.tsx
+++ src/page/public/Home/Home.tsx
@@ -2,8 +2,26 @@
 
 export default function Home() {
   return (
-    <div>
-      Home Page
+   <div className="h-screen flex items-center justify-center dark:bg-gray-800">
+      
+      {/* Botón con Tooltip de Flowbite */}
+      <button 
+        data-tooltip-target="tooltip-default" 
+        type="button" 
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Pasa el mouse aquí
+      </button>
+
+      <div 
+        id="tooltip-default" 
+        role="tooltip" 
+        className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+      >
+        ¡Flowbite + Tailwind v4 funcionando!
+        <div className="tooltip-arrow" data-popper-arrow></div>
+      </div>
+
     </div>
   )
 }
